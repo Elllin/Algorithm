c1f75111a59c5bf7ab54c4a6bafe90c5de413c56
Elllin <noto01@naver.com>
2020-07-08 11:31:42 +0900

    Refactor : 변수 선언 변경

--- leetcode/happyNumber.js
+++ leetcode/happyNumber.js
@@ -1,11 +1,12 @@
 var isHappy = function (n, previousNum = []) {
   let result = false;
 
-  let numArr = n
+  const numArr = n
     .toString()
     .split("")
     .map((value) => value * value);
-  let sum = numArr.reduce((acc, value) => acc + value, 0);
+
+  const sum = numArr.reduce((acc, value) => acc + value, 0);
 
   if (sum === 1) return true;
   else if (previousNum.includes(sum)) return false;
