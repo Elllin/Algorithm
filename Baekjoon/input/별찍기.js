11463f05e3f2526806d3b5c23a7b745521f8effc
Elllin <noto01@naver.com>
2020-07-20 15:19:15 +0900

    Feat : 별찍기

---
+++ Baekjoon/input/별찍기.js
@@ -0,0 +1,21 @@
+const readline = require("readline");
+const { format } = require("path");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+let input;
+rl.on("line", function (line) {
+  input = parseInt(line);
+  rl.close();
+}).on("close", function () {
+  let answer = [];
+  for (let i = 0; i < input - 1; i++) {
+    answer.push(" ");
+  }
+  for (let i = input - 1; i >= 0; i--) {
+    answer[i] = "*";
+    console.log(answer.join(""));
+  }
+  process.exit();
+});
