9841f1ab46100e373bee7f728bc2b51a2ca7593c
Elllin <noto01@naver.com>
2020-07-21 08:58:37 +0900

    Feat : 별찍기-16(트리모양) 구현

---
+++ Baekjoon/input/별찍기-16.js
@@ -0,0 +1,25 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+let input;
+rl.on("line", function (line) {
+  input = parseInt(line);
+  rl.close();
+}).on("close", function () {
+  for (let i = 1; i <= input; i++) {
+    let answer = "";
+    for (let j = 0; j < input - i; j++) {
+      answer += " ";
+    }
+
+    for (let k = 0; k < i * 2 - 1; k++) {
+      if (k % 2 === 0) answer += "*";
+      else answer += " ";
+    }
+    console.log(answer);
+  }
+
+  process.exit();
+});
