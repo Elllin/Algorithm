77ebff6ba1a374d7daff5f783d36a9e3daeebc37
Elllin <noto01@naver.com>
2020-07-22 15:43:30 +0900

    Feat : 스택을 이용한 괄호찾기 문제

---
+++ Baekjoon/stack/괄호.js
@@ -0,0 +1,27 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+let input = [];
+rl.on("line", function (line) {
+  input.push(line);
+}).on("close", function () {
+  let answer = 0;
+  input.shift();
+  input.forEach((v) => {
+    const stack = [];
+    stack.push(v[0]);
+    for (let i = 1; i < v.length; i++) {
+      //   console.log(stack[stack.length - 1] + v[i]);
+      if (!stack.length) stack.push(v[i]);
+      else if (stack[stack.length - 1] + v[i] === "()") {
+        stack.pop();
+      } else stack.push(v[i]);
+    }
+    if (stack.length) console.log("NO");
+    else console.log("YES");
+  });
+
+  process.exit();
+});
